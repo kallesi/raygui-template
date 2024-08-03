1add87cdddbdd57a4dfdade8760436d0334573a5
Kalle <kalle.t.sipila@gmail.com>
2024-08-03 23:48:57 +0800

    Reformat

--- main.cpp
+++ main.cpp
@@ -5,27 +5,32 @@
 
 int main()
 {
-    InitWindow(400, 200, "raygui - controls test suite");
+    int window_width{800};
+    int window_height{400};
+    InitWindow(window_width, window_height, "kallesi - raygui template");
     SetTargetFPS(60);
 
     bool showMessageBox = false;
 
     while (!WindowShouldClose())
     {
-        // Draw
-        //----------------------------------------------------------------------------------
+
         BeginDrawing();
-            ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
 
-            if (GuiButton((Rectangle){ 24, 24, 120, 30 }, "#191#Show Message")) showMessageBox = true;
+        ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
 
-            if (showMessageBox)
-            {
-                int result = GuiMessageBox((Rectangle){ 85, 70, 250, 100 },
-                    "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
+        if (GuiButton((Rectangle){50, 50, 240, 60}, "#191#Show Message"))
+        {
+            showMessageBox = true;
+        }
 
-                if (result >= 0) showMessageBox = false;
-            }
+        if (showMessageBox)
+        {
+            int result = GuiMessageBox((Rectangle){170, 140, 500, 200}, "#191#Message Box", "Message", "Yes");
+
+            if (result >= 0)
+                showMessageBox = false;
+        }
 
         EndDrawing();
     }
